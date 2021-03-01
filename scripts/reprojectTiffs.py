0d3983a651ca53b2c9af9d658de8e27502e266fb
Kim Durante <kimberlydurante@gmail.com>
2021-03-01 06:42:01 -0800

    Create reprojectTiffs.py

---
+++ scripts/reprojectTiffs.py
@@ -0,0 +1,17 @@
+import os
+import fnmatch
+
+INPUT_FOLDER="SFMAPS"
+OUTPUT_FOLDER= "wgs84"
+
+def findRasters (path, filter):
+    for root, dirs, files in os.walk(path):
+        for file in fnmatch.filter(files, filter):
+            yield file
+
+for raster in findRasters(INPUT_FOLDER, '*.tif'):
+    newFile = raster[:-4]
+    inRaster = INPUT_FOLDER + '/' + raster
+    outRaster = OUTPUT_FOLDER +'/' + newFile + '_wgs84.tif'
+    cmd = 'gdalwarp -t_srs EPSG:4326 -te xmin ymin xmax ymax -dstalpha %s %s' % (inRaster, outRaster)
+    os.system(cmd)
