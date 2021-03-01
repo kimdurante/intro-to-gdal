4705492074579cc27542a9cbd3994e7a460599fb
Kim Durante <kimberlydurante@gmail.com>
2021-03-01 11:05:54 -0800

    Update shp2geojson.py

--- scripts/shp2geojson.py
+++ scripts/shp2geojson.py
@@ -1,13 +1,17 @@
 import os
 import fnmatch
 
-INPUT_FOLDER="SFMAPS"
+INPUT_FOLDER="."
 OUTPUT_FOLDER= "geojson"
+if not os.path.exists('geojson'):
+    os.makedirs('geojson')
+
 
 def findRasters (path, filter):
     for root, dirs, files in os.walk(path):
         for file in fnmatch.filter(files, filter):
             yield file
+        break
 
 for shapefile in findRasters(INPUT_FOLDER, '*.shp'):
     newFile = shapefile[:-4]
