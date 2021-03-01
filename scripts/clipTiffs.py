e2eaeb9acf68f39ff58737a52922d56403df72c4
Kim Durante <kimberlydurante@gmail.com>
2021-03-01 06:42:38 -0800

    Create clipTiffs.py

---
+++ scripts/clipTiffs.py
@@ -0,0 +1,17 @@
+import os
+import fnmatch
+
+CLIP= "sf_94109.geojson"
+INPUT_FOLDER="SFMAPS"
+OUTPUT_FOLDER= "clipped"
+
+def findRasters (path, filter):
+    for root, dirs, files in os.walk(path):
+        for file in fnmatch.filter(files, filter):
+            yield file
+
+for raster in findRasters(INPUT_FOLDER, '*.tif'):
+    inRaster = INPUT_FOLDER + '/' + raster
+    outRaster = OUTPUT_FOLDER + '/clip_' + raster
+    cmd = 'gdalwarp -q -cutline %s -crop_to_cutline -dstalpha %s %s' % (CLIP, inRaster, outRaster)
+    os.system(cmd)
