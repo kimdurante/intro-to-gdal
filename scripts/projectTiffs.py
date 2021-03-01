d3137f08a3f2fef0652651c090913535c845933e
Kim Durante <kimberlydurante@gmail.com>
2021-03-01 10:58:26 -0800

    Update and rename reprojectTiffs.py to projectTiffs.py

--- scripts/projectTiffs.py
+++ scripts/projectTiffs.py
@@ -1,13 +1,16 @@
 import os
 import fnmatch
 
-INPUT_FOLDER="SFMAPS"
+INPUT_FOLDER="."
 OUTPUT_FOLDER= "wgs84"
+if not os.path.exists('wgs84'):
+    os.makedirs('wgs84')
 
 def findRasters (path, filter):
     for root, dirs, files in os.walk(path):
         for file in fnmatch.filter(files, filter):
             yield file
+        break
 
 for raster in findRasters(INPUT_FOLDER, '*.tif'):
     newFile = raster[:-4]
