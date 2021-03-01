d1015f76969071f6475ac81ef4dde469bc64767d
Kim Durante <kimberlydurante@gmail.com>
2021-03-01 09:22:45 -0800

    Update reprojectTiffs.py

--- scripts/reprojectTiffs.py
+++ scripts/reprojectTiffs.py
@@ -13,5 +13,5 @@ for raster in findRasters(INPUT_FOLDER, '*.tif'):
     newFile = raster[:-4]
     inRaster = INPUT_FOLDER + '/' + raster
     outRaster = OUTPUT_FOLDER +'/' + newFile + '_wgs84.tif'
-    cmd = 'gdalwarp -t_srs EPSG:4326 -te xmin ymin xmax ymax -dstalpha %s %s' % (inRaster, outRaster)
+    cmd = 'gdalwarp -t_srs EPSG:4326 -te xmin ymin xmax ymax -dstalpha -overwrite %s %s' % (inRaster, outRaster)
     os.system(cmd)
